864493435e6f2d4b7456c65e61ecb28a1d00ae2d
Mohammad Yasin Khosh manesh <yasin.dev81@gmail.com>
2024-09-01 21:44:33 +0330

    cleaning

--- html, css/0.interduction.py
+++
@@ -1,6 +0,0 @@
-
-# html: structure
-# css: presentation, appearance
-# javascript: dynamism, action
-
-"HTML = HyperText Markup Language"
--- python/lambda.py
+++
@@ -1,7 +0,0 @@
-# def in line 4 == lambda in line 7
-scores = [1, 5, 12, 16, 20]
-
-def is_more_than_15(score) :
-    return (15 <= score) 
-
-lambda score: (15 <= score)
